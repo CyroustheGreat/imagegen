5713f79687f5c09973c8f346c6e9e58fe97dcffd
Amin <aaeinmehr@gmail.com>
2023-10-30 18:51:07 -0700

    headerIsDone

---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+// it's serves as an export function for the CreatePost.jsx and Home.jsx files
+
+import Home from "./Home";
+import CreatePost from "./CreatePost";
+
+export {
+    Home,
+    CreatePost,
+}
