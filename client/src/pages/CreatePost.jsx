af383d47bdcea6a5ff0bfdf23a5f588325bc5237
Amin <aaeinmehr@gmail.com>
2023-10-30 20:45:31 -0700

    creatingPost:statesarecreated

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandmPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
+  const navigate = useNavigate(); //allows us to navigate to our home page when a post is created
+
+  const [form, setForm] = useState({ name: "", prompt: "", photo: "" });
+
+  const [generatingImg, setGeneratingImg] = useState(false); //when we are making contact with our API
+
+  const [loading, setLoading] = useState(false);
+
   return <div>Create</div>;
 };
-
 export default CreatePost;
